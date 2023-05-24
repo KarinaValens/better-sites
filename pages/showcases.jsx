5eaf428cfd818ab3cd240614f6df61f5a8456662
Karina Valens <karyvalens@yahoo.com>
2023-05-25 00:55:36 +0200

    fixing names problems showcases and showCases

--- pages/showcases.jsx
+++ pages/showcases.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { AppContext } from "../components/context/AppContext";
 import ShowcasesCards from "../components/showcases/ShowcasesCards";
-import ShowcasesCTA from "../components/showcases/ShowcasesCTA";
+import ShowcasesCTA from "../components/showcases/ShowCasesCTA";
 
 export default function Showcases() {
   const { show } = useContext(AppContext);
